9603edde4794dfad6e6705d2393526604fd78b91
Sifan Ye, on his MBP <ysf199711@126.com>
2019-03-14 00:08:51 -0400

    Use Threading.Task

--- Unity Client/Unity/demo/Assets/Client.cs
+++ Unity Client/Unity/demo/Assets/Client.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
 
 using NetMQ;
 using NetMQ.Sockets;
@@ -9,50 +9,43 @@ using NetMQ.Sockets;
 using UnityEngine;
 
 
-public class ParallelDealer{
-	private readonly Thread dealer;
-	private bool dealerStopped;
-	private string sendMsg = "";
+public class Client : MonoBehaviour {
+	public Light light;
+	public bool lightIsOn = false; 
 
-	public delegate void MessageDelegate(string obj);
-	private readonly MessageDelegate messageDelegate;
+	private bool dealerIsStarted = true;
 
-	public ParallelDealer(MessageDelegate msgDelegate){
-		dealer = new Thread(DealerWorker);
-		messageDelegate = msgDelegate;
-	}
-
-	private void DealerWorker(){
-		AsyncIO.ForceDotNet.Force();
-
+	void dealerListener(){
 		var socket = new DealerSocket();
 		socket.Options.Identity = Encoding.UTF8.GetBytes("client_" + System.Guid.NewGuid().ToString());
 		socket.Connect("tcp://localhost:5580");
 
 		try{
-			var workerStarted = false;
-			while(!dealerStopped){
+			var workerIsStarted = false;
+			while(dealerIsStarted){
 				// Wait for worker
-				while(!workerStarted){
+				while(!workerIsStarted){
 					socket.SendFrame("Client Ready");
 					string m;
 					if (socket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out m)) {
-						
 						if(m == "Worker Ready"){
-							workerStarted = true;
+							Debug.Log("Worker Ready");
+							workerIsStarted = true;
+							socket.SendFrame("Light is off");
 						}
 					}
 				}
 				string msg;
 				if (socket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out msg)) {
-					messageDelegate(msg);
+					Debug.Log("Received: " + msg);
+					if(msg == "on"){
+						lightIsOn = true;
+						socket.SendFrame("Light is on");
+					}else if(msg == "off"){
+						lightIsOn = false;
+						socket.SendFrame("Light is off");
+					}
 				}
-				if(!string.Equals(sendMsg, "")){
-					socket.SendFrame(sendMsg);
-					Debug.Log("Sent: " + sendMsg);
-					sendMsg = "";
-				}
-				Thread.Sleep(1000);
 			}
 		}finally{
 			if (socket != null) {
@@ -62,45 +55,11 @@ public class ParallelDealer{
 		}
 	}
 
-	public void Update(string msg){
-		sendMsg = msg;
-	}
-
-	public void Start(){
-		dealerStopped = false;
-		dealer.Start();
-	}
-
-	public void Stop(){
-		dealerStopped = true;
-
-		dealer.Join();
-	}
-}
-
-public class Client : MonoBehaviour {
-	public ParallelDealer dealer;
-	public Light light;
-	public bool lightIsOn = false; 
-
-	void MessageHandler(string msg){
-		Debug.Log ("Received: " + msg);
-		if(string.Equals(msg, "on")){
-			lightIsOn = true;
-			dealer.Update("Light is on");
-		}else if(string.Equals(msg, "off")){
-			lightIsOn = false;
-			dealer.Update("Light is off");
-		}else{
-			return;
-		}
-	}
-
 	// Use this for initialization
 	void Start () {
-		dealer = new ParallelDealer (MessageHandler);
-		dealer.Start();
-		dealer.Update ("Light is off");
+		lightIsOn = false;
+		Task task = new Task (async() => dealerListener ());
+		task.Start ();
 	}
 	
 	// Update is called once per frame
@@ -113,6 +72,6 @@ public class Client : MonoBehaviour {
 	}
 
 	void OnDestroy(){
-		dealer.Stop();
+		dealerIsStarted = false;
 	}
 }
