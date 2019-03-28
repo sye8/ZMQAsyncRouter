f6bf6d1590a7901ba96c88f658d5b67bc87fad65
Li Zhang <lzhang66@u.rochester.edu>
2019-03-27 21:55:05 -0400

    Xmas lights

--- UnityClient/Unity/demo/Assets/Client.cs
+++ UnityClient/Unity/demo/Assets/Client.cs
@@ -1,81 +1,113 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+
+using UnityEngine;
 
 using NetMQ;
 using NetMQ.Sockets;
 
-using UnityEngine;
+public class Client : MonoBehaviour
+{
+    // For async router comm
+    private string colorStr = "";
+    private bool dealerIsStarted = false;
+
+    // For color changing
+    public Renderer DraggableCube;
+
+    async void dealerListener()
+    {
+        AsyncIO.ForceDotNet.Force();
+
+        var socket = new DealerSocket();
+        socket.Options.Identity = Encoding.UTF8.GetBytes("client_" + System.Guid.NewGuid().ToString());
+        socket.Connect("tcp://172.24.1.1:5580");
+
+        try
+        {
+            var workerIsStarted = false;
+            while (dealerIsStarted)
+            {
+                // Wait for worker
+                while (!workerIsStarted)
+                {
+                    socket.SendFrame("Client Ready");
+                    string m;
+                    if (socket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out m))
+                    {
+                        if (m == "Worker Ready")
+                        {
+                            Debug.Log("Worker Ready");
+                            workerIsStarted = true;
+                            socket.SendFrame("Xmas Lights!");
+                        }
+                    }
+                }
+                string msg;
+                if (socket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out msg))
+                {
+                    Debug.Log("Received: " + msg);
+                    colorStr = msg;
+                    socket.SendFrame("Will change color to " + msg);
+                }
+                await Task.Delay(500);
+            }
+        }
+        finally
+        {
+            if (socket != null)
+            {
+                socket.SendFrame("Client Stopping");
+                ((IDisposable)socket).Dispose();
+            }
+        }
+    }
+
+    void Start()
+    {
+        colorStr = "white";
+        dealerIsStarted = true;
+        Task task = new Task(async () => dealerListener());
+        task.Start();
+    }
 
 
-public class Client : MonoBehaviour {
-	public Light light;
-	public bool lightIsOn = false; 
+    void Update()
+    {
+        Color newColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);   // white (default)
+        // Parsing color from server code
+        if (string.Equals("red", colorStr))
+        {
+            newColor = new Color(1.0f, 0f, 0f, 1.0f);  // red
+        }
+        else if (string.Equals("green", colorStr))
+        {
+            newColor = new Color(0f, 1.0f, 0f, 1.0f); // green
+        }
+        else if (string.Equals("blue", colorStr))
+        {
+            newColor = new Color(0f, 0f, 1.0f, 1.0f); // blue
+        }
+        else if (string.Equals("yellow", colorStr))
+        {
+            newColor = new Color(1.0f, 0.92f, 0.016f, 1.0f); // yellow
+        }
+        else if (string.Equals("magenta", colorStr))
+        {
+            newColor = new Color(1f, 0f, 1.0f, 1.0f); // mangeta
+        }
+        else
+        {
+            Debug.Log("Color " + colorStr + " undefined. Using black.");
+            newColor = new Color(0f, 0f, 0f, 1.0f);   // black
+        }
 
-	private bool dealerIsStarted = false;
+        // Changing color
+        DraggableCube = GetComponent<Renderer>();
+        DraggableCube.material.color = newColor;
 
-	async void dealerListener(){
-		AsyncIO.ForceDotNet.Force();
-
-		var socket = new DealerSocket();
-		socket.Options.Identity = Encoding.UTF8.GetBytes("client_" + System.Guid.NewGuid().ToString());
-		socket.Connect("tcp://localhost:5580");
-
-		try{
-			var workerIsStarted = false;
-			while(dealerIsStarted){
-				// Wait for worker
-				while(!workerIsStarted){
-					socket.SendFrame("Client Ready");
-					string m;
-					if (socket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out m)) {
-						if(m == "Worker Ready"){
-							Debug.Log("Worker Ready");
-							workerIsStarted = true;
-							socket.SendFrame("Light is off");
-						}
-					}
-				}
-				string msg;
-				if (socket.TryReceiveFrameString(TimeSpan.FromSeconds(3), out msg)) {
-					Debug.Log("Received: " + msg);
-					if(msg == "on"){
-						lightIsOn = true;
-						socket.SendFrame("Light is on");
-					}else if(msg == "off"){
-						lightIsOn = false;
-						socket.SendFrame("Light is off");
-					}
-				}
-				await Task.Delay(500);
-			}
-		}finally{
-			if (socket != null) {
-				socket.SendFrame("Client Stopping");
-				((IDisposable)socket).Dispose();
-			}
-		}
-	}
-
-	// Use this for initialization
-	void Start () {
-		lightIsOn = false;
-		dealerIsStarted = true;
-		Task task = new Task (async() => dealerListener ());
-		task.Start ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (lightIsOn) {
-			light.enabled = true;
-		} else {
-			light.enabled = false;
-		}
-	}
-
-	void OnDestroy(){
-		dealerIsStarted = false;
-	}
+    }
 }
